d07b9b38974acb54be4f172b44812be42670836d
Lukas Wagner <lwagner94@posteo.at>
2022-01-30 14:16:55 +0100

    Add criterion for basic benchmarking

---
+++ benches/runtime.rs
@@ -0,0 +1,37 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use wasmut::{
+    policy::ExecutionPolicy,
+    runtime::{create_runtime, wasmer::WasmerRuntime},
+    wasmmodule::WasmModule,
+};
+
+fn create_rt(module: WasmModule) {
+    create_runtime(module);
+}
+
+fn exec_module(runtime: &WasmerRuntime) {}
+
+fn criterion_benchmark(c: &mut Criterion) {
+    let large_module = WasmModule::from_file("testdata/simple_rust/test.wasm").unwrap();
+    let small_module = WasmModule::from_file("testdata/simple_add/test.wasm").unwrap();
+
+    let mut large_rt = create_runtime(large_module.clone()).unwrap();
+    let mut small_rt = create_runtime(small_module.clone()).unwrap();
+
+    c.bench_function("WasmerRuntime::new() small module", |b| {
+        b.iter(|| create_rt(black_box(small_module.clone())))
+    });
+    c.bench_function("WasmerRuntime::new() large module", |b| {
+        b.iter(|| create_rt(black_box(large_module.clone())))
+    });
+
+    c.bench_function("WasmerRuntime::call_test_function() large module", |b| {
+        b.iter(|| large_rt.call_test_function(ExecutionPolicy::RunUntilReturn))
+    });
+    c.bench_function("WasmerRuntime::call_test_function() small module", |b| {
+        b.iter(|| small_rt.call_test_function(ExecutionPolicy::RunUntilReturn))
+    });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
