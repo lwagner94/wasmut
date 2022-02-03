301693bf224792eec7efbead7348d0e44f0d80f7
Lukas Wagner <lwagner94@posteo.at>
2022-02-03 18:36:50 +0100

    Disable branch coverage, add local coverage script

---
+++ coverage.sh
@@ -0,0 +1,8 @@
+export CARGO_INCREMENTAL=0
+export RUSTFLAGS="-Zprofile -Ccodegen-units=1 -Copt-level=0 -Clink-dead-code -Coverflow-checks=off -Zpanic_abort_tests -Cpanic=abort"
+export RUSTDOCFLAGS="-Cpanic=abort"
+
+
+cargo +nightly test
+
+grcov . -s . --binary-path ./target/debug/ -t html --llvm --filter covered --ignore-not-existing  -o ./target/debug/coverage
